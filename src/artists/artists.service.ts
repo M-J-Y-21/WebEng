b87bfb133ec763f31f2aabbe4ba3286631a6f723
Matan <matanyakobovich@yahoo.com>
2023-01-02 09:56:16 +0200

    3rd point of assingment M1 Req Implementation (Pt. 2) Finished Retrieval
    Now able to correclty retrieve songs by the artist id and artist name.

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -5,13 +5,14 @@ const prisma = new PrismaClient();
 
 @Injectable()
 export class ArtistsService {
+  /**
+   * In first part of function we assume that the idOrName is an id.
+   * If it is not an id, we assume it is a name.
+   * @param idOrName
+   * @returns
+   */
   async getSongsByArtist(idOrName: string): Promise<Song[]> {
-    // First one get all songs of artist given an artist Id
-    // right now flawed as will only work I think if that song
-    // has only that one artist Id but if it has two including that one
-    // I think it'll fail check? Yes this it true
-    // has below solves issue
-    let songs = [];
+    let songs: Song[] = [];
     const songsById = await prisma.song.findMany({
       where: {
         artist_ids: {
@@ -24,83 +25,29 @@ export class ArtistsService {
     }
     /**
      * If the idOrName is not an id, it is a name.
-     * Find the first artist by the matching name
-     * and return the songs by id.
-     * Better solution return songs of all artists with matching name.
-     * Did that below.
+     * Return songs of all artists with matching name.
      */
     const idArtistByName = await prisma.artist.findMany({
       where: {
         name: idOrName,
       },
     });
-    console.log(idArtistByName);
     if (idArtistByName.length > 0) {
       const artistIds = idArtistByName
         .map((artist) => artist.id)
         .map((id) => id.toString());
 
       for (let index = 0; index < artistIds.length; index++) {
-        songs.push(
+        songs = songs.concat(
           await prisma.song.findMany({
             where: {
-              artist_ids: { equals: artistIds[index] },
+              artist_ids: { has: artistIds[index] },
+              name: { notIn: songs.map((song) => song.name) },
             },
           }),
         );
-        console.log(songs);
       }
       return songs;
-
-      // const songsByName = await prisma.song.findMany({
-      //   where: {
-      //     artist_ids: { equals: artistIds },
-      //   },
-      // });
-      // console.log(songsByName);
-      // return songsByName;
     }
   }
 }
-
-// to retrieve or delete all songs for a specific artist by artist ID or artist
-// name;
-
-//   async deleteSongsByArtist(idOrName: string): Promise<void> {
-//     // First one delete all songs of artist given an artist Id
-//     const songsById = await prisma.song.findMany({
-//       where: {
-//         artist_ids: {
-//           equals: idOrName,
-//         },
-//       },
-//     });
-//     if (songsById.length > 0) {
-//       songsById.forEach(async song => {
-//         await prisma.song.delete({
-//           where: {
-//             id: song.id,
-//           },
-//         });
-//       });
-//       return;
-//     }
-//     const artists = await prisma.artist.findMany({
-//       where: {
-//         name: idOrName,
-//       },
-//     });
-//     if (artists.length > 0) {
-//       const artistIds = artists.map(artist => artist.id);
-//       const songsByName = await prisma.song.findMany({
-//         where: {
-//           artist_ids: {
-//             in: artistIds.map(id => ({ equals: id })),
-//           },
-//         },
-//       });
-//       songsByName.forEach(async song => {
-//         await prisma.song.delete({
-//           where: {
-
-// }
