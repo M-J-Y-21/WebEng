ce3f8adbf47010fcbc0f540b444e5f22ab922851
Matan <matanyakobovich@yahoo.com>
2023-01-13 00:07:20 +0100

    Someone forgot a very important await.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -4,7 +4,7 @@ import { parse } from 'json2csv';
 
 @Controller('artists')
 export class ArtistsController {
-  constructor(private readonly artistsService: ArtistsService) { }
+  constructor(private readonly artistsService: ArtistsService) {}
 
   private sendResponse(res, contentType, data) {
     if (contentType == 'text/csv') {
@@ -14,52 +14,31 @@ export class ArtistsController {
       res.send(data);
     }
   }
-  
+
   // REQ 3 Get
   @Get('songs')
-  async getSongsByArtist(
-    @Query('id') id: string,
-    @Query('name') name: string,
-    @Headers('Content-Type') contentType: string,
-    @Res() res
-  ) {
+  async getSongsByArtist(@Query('id') id: string, @Query('name') name: string, @Headers('Content-Type') contentType: string, @Res() res) {
     const songs = await this.artistsService.getSongsByArtist(id, name);
     this.sendResponse(res, contentType, songs);
   }
 
   // REQ 3 Delete
   @Delete('songs')
-  async deleteSongsByArtist(
-    @Query('id') id: string,
-    @Query('name') name: string,
-    @Headers('Content-Type') contentType: string,
-    @Res() res
-  ) {
+  async deleteSongsByArtist(@Query('id') id: string, @Query('name') name: string, @Headers('Content-Type') contentType: string, @Res() res) {
     const songs = await this.artistsService.deleteSongsByArtist(id, name);
     this.sendResponse(res, contentType, songs);
   }
 
   // REQ 4
   @Get('summary')
-  async getSummary(
-    @Query('id') id: string,
-    @Query('name') name: string,
-    @Headers('Content-Type') contentType: string,
-    @Res() res
-  ) {
-    const summary = this.artistsService.getSummary(id, name);
+  async getSummary(@Query('id') id: string, @Query('name') name: string, @Headers('Content-Type') contentType: string, @Res() res) {
+    const summary = await this.artistsService.getSummary(id, name);
     this.sendResponse(res, contentType, summary);
   }
 
   // REQ 6
   @Get('')
-  async getTopArtists(
-    @Query('year') year: number,
-    @Query('limit') limit: number,
-    @Query('skip') skip: number,
-    @Headers('Content-Type') contentType: string,
-    @Res() res
-  ) {
+  async getTopArtists(@Query('year') year: number, @Query('limit') limit: number, @Query('skip') skip: number, @Headers('Content-Type') contentType: string, @Res() res) {
     const artists = await this.artistsService.getTopArtists(year, limit, skip);
     this.sendResponse(res, contentType, artists);
   }
